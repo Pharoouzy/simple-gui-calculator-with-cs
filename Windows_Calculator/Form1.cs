57fe59af201b1ead77619c47e0d2191578628c02
Pharoouzy <pharoouzy@yahoo.com>
2018-12-03 19:16:44 +0100

    Windows Calculator

---
+++ Windows_Calculator/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_Calculator
+{
+    public partial class Calculator : Form
+    {
+        Double value = 0;
+        String operation = "";
+        bool operation_pressed = false;
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if((result.Text == "0") || (operation_pressed))
+            {
+                result.Clear();
+            }
+            operation_pressed = false;
+            Button b = (Button)sender;
+            result.Text = result.Text + b.Text;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            result.Text = "0";
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            operation = b.Text;
+            value = Double.Parse(result.Text);
+            operation_pressed = true;
+            eqn.Text = value + " " + operation;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            eqn.Text = "";
+            switch(operation)
+            {
+                case "+":
+                    result.Text = (value + Double.Parse(result.Text)).ToString();
+                    break;
+
+                case "-":
+                    result.Text = (value - Double.Parse(result.Text)).ToString();
+                    break;
+                case "*":
+                    result.Text = (value * Double.Parse(result.Text)).ToString();
+                    break;
+                case "/":
+                    result.Text = (value / Double.Parse(result.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }//end of switch statement.
+            //operation_pressed = false;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            result.Text = "0";
+            value = 0;
+        }
+    }
+}
